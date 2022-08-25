813e1ed9d3109c018de58b27ee0174ddcb0ccb77
Sahir Haque <ayaanhaque07@gmail.com>
2022-08-26 01:25:08 +0530

    Second Commit

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,21 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import Layout from "../components/Layout";
+import ProductItem from "../components/ProductItem";
+import data from "../utils/data";
+// import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div>
-      <Head>
-        <h1 className="text-3xl bg-red-300"> Next Tailwind css</h1>
-      </Head>
-    </div>
+    <>
+      <Layout title="Home Page">
+        Home Page
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+          {data.products.map((product) => (
+            <ProductItem product={product} key={product.slug} />
+          ))}
+        </div>
+      </Layout>
+    </>
   );
 }
